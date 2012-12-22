4055d78e1241996fb251e8e7e4afc3ade77897b8
Jordan Byron <jordan.byron@gmail.com>
2012-12-21 20:40:43 -0500

    Parallax'in

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(function(){
-  $('#hero').css('height', $(window).height() + 10);
+  $('#hero').css('height', $(window).height());
   $('#hero h1').css('opacity', 1);
+  $('#hint').css('opacity', 1);
+  
+  $('[data-speed]').each(function(){
+    var $bgobj = $(this); // assigning the object
+    $(window).scroll(function() {
+      var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+      // Put together our final background position
+      var coords = '50% '+ yPos + 'px';
+      // Move the background
+      $bgobj.css({ backgroundPosition: coords });
+    });
+  });
 });
